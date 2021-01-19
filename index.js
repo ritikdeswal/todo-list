44b6768c8b148bd0323b763281d572a8f263e09d
Ritik Deswal <47329582+ritikdeswal@users.noreply.github.com>
2021-01-19 19:32:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Arrays to keep track of each task's state
+const taskTitles = [];
+const taskComplete = [];
+
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
+function newTask(title) {
+  taskTitles.push(title);
+  taskComplete.push(false);
+}
+
+// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+function completeTask(taskIndex) {
+  taskComplete[taskIndex] = true;
+}
+
+// Print the state of a task to the console in a nice readable way
+function logTaskState(taskIndex) {
+  const title = taskTitles[taskIndex];
+  const complete = taskComplete[taskIndex];
+  console.log(`${title} has${complete ? " " : " not "}been completed`);
+}
+
+// DRIVER CODE BELOW
+
+newTask("Clean Cat Litter"); // task 0
+newTask("Do Laundry"); // task 1
+
+logTaskState(0); // Clean Cat Litter has not been completed
+completeTask(0);
+logTaskState(0); // Clean Cat Litter has been completed
